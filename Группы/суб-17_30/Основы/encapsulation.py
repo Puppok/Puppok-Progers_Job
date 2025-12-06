38df2afe7350dce4b8027493240444661014c957
Puppok <kravchenko19971808@gmail.com>
2025-12-06 20:26:50 +0800

    6.12.2025

--- Группы/суб-17_30/Основы/encapsulation.py
+++ Группы/суб-17_30/Основы/encapsulation.py
@@ -63,6 +63,39 @@ myHouse.secretItems = 'sosok' # успешно
 F = C * 9/5 + 32, 
 C = (F - 32) * 5/9
 """
+class Temperature:
+    def __init__(self, celsius: float):
+        self._celsius = celsius
+
+    @property
+    def celsius(self):
+        return self._celsius
+
+    @celsius.setter
+    def celsius(self, value: float):
+        if value < -273.15:
+            raise AttributeError('Can\'t be lower than -273.15')
+
+        self._celsius = value
+
+    @property
+    def fahrenheit(self):
+        return self._celsius * 1.8 + 32
+
+    @fahrenheit.setter
+    def fahrenheit(self, value: float):
+        self._celsius = (value - 32) * 1.8
+
+# temp = Temperature(50)
+# print(temp.celsius)
+# print(temp.fahrenheit)
+# # temp.celsius = -273.19
+# temp.celsius = 128
+# print(temp.celsius)
+# temp.farenheit = 3_275
+# print(temp.farenheit)
+
+
 
 """
 ЗАДАЧА 2: Класс "Ученик"
@@ -75,6 +108,54 @@ C = (F - 32) * 5/9
 - метод add_grade(grade) - добавление оценки (от 2 до 5)
 - property average_grade - средний балл (только для чтения)
 """
+class Student:
+    def __init__(self, name: str, age: int):
+        self._name = name
+        self._age = age
+        self._grades: list[int] = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value: int):
+        if value < 6:
+            raise AttributeError('Can\'t be lower than 6')
+
+        if value > 18:
+            raise AttributeError('Can\'t be higher than 18')
+
+        self._age = value
+
+    @property
+    def average_grade(self):
+        return sum(self._grades) / len(self._grades)
+
+    def add_grade(self, grade: int):
+        if grade < 2 or grade > 5:
+            raise AttributeError('Can\'t be lower than 2 or higher than 5')
+
+        self._grades.append(grade)
+
+# tom = Student('tom', 18)
+# tom.add_grade(5)
+# tom.add_grade(4)
+# tom.add_grade(3)
+# tom.add_grade(4)
+# # tom.add_grade(1)
+# tom.add_grade(2)
+# print(tom.average_grade)
+# print(tom.age)
+# print(tom.name)
+# tom.age = 15
+# print(tom.age)
+
+
 
 """
 ЗАДАЧА 3: Класс "Прямоугольник"
@@ -85,6 +166,9 @@ C = (F - 32) * 5/9
 - property perimeter - периметр (только для чтения)
 - метод is_square() - проверка, является ли квадратом
 """
+class Rectangle:
+    pass
+
 
 """
 ЗАДАЧА 4: Класс "Пользователь"
