79dda293a66fcbded94e44f204c81bce8f75e6fb
Puppok <kravchenko19971808@gmail.com>
2025-12-08 16:30:14 +0800

    8.12.2025

---
+++ Группы/пн-ср_15/pyGame/1_Basics.py
@@ -0,0 +1,28 @@
+import pygame
+
+icon = pygame.image.load('icon.png') # подгрузка картинки 32х32
+blue_bg = (0, 0, 255) # синий цвет (для заднего фона)
+
+pygame.init() # инициализация библиотеки
+
+# screen = pygame.display.set_mode((800, 600)) # создание окна
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+pygame.display.set_caption('Писька бобра') # заголовок окна
+pygame.display.set_icon(icon) # задаем иконку окна (слева от заголовка)
+
+gameStarted = True # состояние игры (запущена)
+while gameStarted: # главный цикл (пока игра запущена)
+    # === 1. Главный цикл игры ===
+    for event in pygame.event.get(): # считываем все события, во время работы игры
+        if event.type == pygame.QUIT: # если событие - выход из игры (кнопка закрытия окна)
+            gameStarted = False # останавливаем игру
+
+    # === 2. Основная логика игры ===
+        # просчеты движения
+        # просчеты физики
+
+    # === 3. Отрисовка всех элементов, обновление ===
+    screen.fill(blue_bg) # заливка окна цветом
+    pygame.display.flip() # обновление экрана
+
+pygame.quit() # завершение работы библиотеки (очистка памяти)
