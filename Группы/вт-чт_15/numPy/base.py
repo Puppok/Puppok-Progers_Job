915cc1b6306f9b029cd830d9dfb282204dbfd40e
Puppok <kravchenko19971808@gmail.com>
2025-12-09 17:24:21 +0800

    [9.12.2025] - refactor

--- Группы/вт-чт_15/numPy/base.py
+++ Группы/вт-чт_15/numPy/base.py
@@ -4,37 +4,37 @@ import time
 amount = 100_000_000
 
 # Просчет времени работы через python
-# py_list = list(range(amount))
-# py_start = time.time()
-# py_result = [x * 2 for x in py_list]
-# py_time = time.time() - py_start
-# print(f'Time python: {py_time:.4f} сек')
+py_list = list(range(amount))
+py_start = time.time()
+py_result = [x * 2 for x in py_list]
+py_time = time.time() - py_start
+print(f'Time python: {py_time:.4f} сек')
 
 # Просчет времени работы через numPy
-# np_list = np.arange(amount)
-# np_start = time.time()
-# np_result = np_list * 2
-# np_time = time.time() - np_start
-# print(f'Time numPy: {np_time:.4f} сек')
-#
-# print(f'Dif in {py_time / np_time:.1f} times')
+np_list = np.arange(amount)
+np_start = time.time()
+np_result = np_list * 2
+np_time = time.time() - np_start
+print(f'Time numPy: {np_time:.4f} сек')
 
-# NumPy
+print(f'Dif in {py_time / np_time:.1f} times')
+
+# === NumPy ===
 # ndarray - N dimensional array
 narray = np.array([1 ,2, 3, 4]) # базовое создание массива
-# print(narray)
-# print(f'Shape: {narray.shape}') # форма массива (строки, столбцы, слои)
-# print(f'Dims: {narray.ndim}') # кол-во измерений
-# print(f'Size: {narray.size}') # размер массива (кол-во элементов)
-# print(f'Types: {narray.dtype}') # тип данных массива
+print(narray)
+print(f'Shape: {narray.shape}') # форма массива (строки, столбцы, слои)
+print(f'Dims: {narray.ndim}') # кол-во измерений
+print(f'Size: {narray.size}') # размер массива (кол-во элементов)
+print(f'Types: {narray.dtype}') # тип данных массива
 
 n2array = np.array([
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ])
-# print(f'Shape: {n2array.shape}')
-# print(f'Dims: {n2array.ndim}')
+print(f'Shape: {n2array.shape}')
+print(f'Dims: {n2array.ndim}')
 
 # Способы создания массивов
 # 1. Из python списков ( [] / list() / () ), c помощью np.array(список)
@@ -66,22 +66,22 @@ print('Диагональ\n', diagonal)
 
 # 4. Последовательности чисел
     # • дипазон
-# arr = np.arange(0, 20, 3) # (start, finish, step)
-# print(arr)
-pep = np.arange(0, 10).reshape(2, 5) # сформировать таблицу из линейного массива
-print(pep)
+arange_arr = np.arange(0, 20, 3) # (start, finish, step)
+print(arange_arr)
+reshape_arr = np.arange(0, 10).reshape(2, 5) # сформировать таблицу из линейного массива
+print(reshape_arr)
 
     # • равномерные N чисел
-# arr = np.linspace(0, 10, 5) # формирует равный диапазон от 0 до 10 из 5 чисел
-# print(arr)
+linarr = np.linspace(0, 10, 5) # формирует равный диапазон от 0 до 10 из 5 чисел
+print(linarr)
 
     # • логарифмическая шкала
-# arr = np.logspace(1, 5, 5)
-# print(arr)
+logarr = np.logspace(1, 5, 5)
+print(logarr)
 
 # 5. Случайные числа
     # • случайность через seed
-np.random.seed(20) # создания seed для генерации
+np.random.seed(20) # создания seed для генерации (одинаковые числа при каждом запуске)
 print(np.random.rand(5))
 
     # • пседвослучайная последовательность
