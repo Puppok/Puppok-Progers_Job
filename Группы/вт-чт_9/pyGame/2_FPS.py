aa0717813283c7e581f44bd7dcf29beeaa8c9568
Puppok <kravchenko19971808@gmail.com>
2025-12-11 15:13:23 +0800

    [11.12.2025] - pyGame 9:00 - 10:30

--- Группы/вт-чт_9/pyGame/2_FPS.py
+++ Группы/вт-чт_9/pyGame/2_FPS.py
@@ -1,10 +1,20 @@
 import pygame as pg
 
+# FPS - кол-во кадров в секунду (frames per second)
+
+# цвета для заливки экрана и текста
+colors = {
+    'white': (255, 255, 255),
+    'black': (0, 0, 0),
+}
+
 pg.init()
 
 screen = pg.display.set_mode((800, 600))
 pg.display.set_caption('Отображение кадров (FPS)')
 clock = pg.time.Clock() # создание часов, для отслеживания кадров и времени
+font = pg.font.Font(None, 100) # создание шрифта для текста (название, размер)
+# формат шрифтов .ttf
 
 isGameStarted = True
 while isGameStarted:
@@ -12,6 +22,19 @@ while isGameStarted:
         if event.type == pg.QUIT:
             isGameStarted = False
 
+    # заполняем экран черным цветом
+    screen.fill(colors['black'])
+
+    # получаем текущий FPS
+    fps = int(clock.get_fps())
+
+    # создаем шаблон текста для отрисовки на экране
+    fps_text = font.render(f'FPS: {fps}', True, colors['white'])
+
+    # отрисовка текста на экране игры
+    screen.blit(fps_text, (10, 10))
+
     pg.display.flip()
+    clock.tick(60) # ограничение работы игры (60 FPS)
 
 pg.quit()
