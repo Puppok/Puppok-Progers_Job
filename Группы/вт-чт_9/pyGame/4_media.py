598ea9a987902655a8a7d8f3ff07b85451fe4825
puppok <kravchenko19971808@gmail.com>
2026-01-15 10:35:23 +0800

    [15.01.2026] Python (PyGame) 9:00-10:30

--- Группы/вт-чт_9/pyGame/4_media.py
+++ Группы/вт-чт_9/pyGame/4_media.py
@@ -17,6 +17,7 @@ crazy_frog_image = pg.image.load('./assets/Crazy_Frog.jpg')
 frog_size = crazy_frog_image.get_rect()
 print(f'Image size: {frog_size.width}x{frog_size.height}')
 
+
 # -- Размеры картинки ---
 # фиксированный размер .scale(image, (width, height))
 scaled_frog = pg.transform.scale(crazy_frog_image, (700, 500))
@@ -31,6 +32,7 @@ relative_frog = pg.transform.scale_by(crazy_frog_image, 0.5)
 # плавное изменение размера (работает как .scale(), но плавно)
 smooth_frog = pg.transform.smoothscale(crazy_frog_image, (100, 100))
 
+
 # --- Вращение картинки ---
 # поворот на определенный градус
 rotate_frog = pg.transform.rotate(crazy_frog_image, 90)
@@ -44,6 +46,7 @@ vertical_frog = pg.transform.flip(crazy_frog_image, False, True)
 # отражение по обеим осям
 flipped_frog = pg.transform.flip(crazy_frog_image, True, True)
 
+
 # --- Картинки с альфа каналом (прозрачность) ---
 # загрузка картинки с альфа каналом
 google_logo = pg.image.load('./assets/google_logo.png').convert_alpha()
@@ -53,18 +56,70 @@ google_logo = pg.image.load('./assets/google_logo.png').convert_alpha()
 google_logo.set_alpha(128) # полупрозрачное
 
 
+# --- Работа с rect ---
+image_icon = pg.image.load('./assets/penguin.png')
+image_icon_rect = image_icon.get_rect() # достаем rect область из картинки
+print(f'Vinni rect: {image_icon_rect}') # <rect(0, 0, 32, 32)>
+
+# Установка позиции через разные вариации точек
+image_icon_rect.center = (400, 300)
+
+# Доступные параметры rect
+# x, y - координаты левого верхнего угла
+print(f'\nIcon coords: {image_icon_rect.x}, {image_icon_rect.y}')
+
+# width, height - размеры (ширина, высота)
+print(f'\nIcon size: {image_icon_rect.width}x{image_icon_rect.height}')
+
+# centerx, centery - координаты центра
+print(f'\nIcon center coords: {image_icon_rect.centerx}, {image_icon_rect.centery}')
+
+# top, left, right, bottom - границы rect
+print(f'\nIcon border: \n'
+      f'Left: {image_icon_rect.left}, \n'
+      f'Right: {image_icon_rect.right}, \n'
+      f'Top: {image_icon_rect.top}, \n'
+      f'Bottom: {image_icon_rect.bottom}')
+
+# Движение rect
+# Сдвинуть на 100 вправо, на 50 вниз (move in-place) .move(x, y)
+image_icon_rect.move_ip(100, 50)
+
+# Создать новый, сразу сдвинутый rect
+new_rect = image_icon_rect.move(50, 300)
+
+# Пример работы
+# Скорость движения
+speed = 5
+
 isActive = True
 while isActive:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             isActive = False
 
+    # массив нажатых клавиш
+    keys = pg.key.get_pressed()
+
+    if keys[pg.K_LEFT]:
+        image_icon_rect.x -= speed
+    if keys[pg.K_RIGHT]:
+        image_icon_rect.x += speed
+    if keys[pg.K_UP]:
+        image_icon_rect.y -= speed
+    if keys[pg.K_DOWN]:
+        image_icon_rect.y += speed
+
+    # ограничение области движения по размерам экрана
+    image_icon_rect.clamp_ip(screen.get_rect())
+
     screen.fill((0, 0, 0))
 
     # blit - Block Image Transfer (перенос блока изображения)
     # .blit(image, (start_x, start_y))
-    screen.blit(relative_frog, (50, 50))
-    screen.blit(google_logo, (300, 50))
+    # screen.blit(relative_frog, (50, 50))
+    # screen.blit(google_logo, (300, 50))
+    screen.blit(image_icon, image_icon_rect)
 
 
     pg.display.flip()
