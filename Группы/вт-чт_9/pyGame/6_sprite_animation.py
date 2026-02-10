50b982264efc5ab4610f6815a8fbf6875456f63e
puppok <kravchenko19971808@gmail.com>
2026-02-10 15:06:51 +0800

    [10.02.2026] Python (PyGame) 9:00-10:30

--- Группы/вт-чт_9/pyGame/6_sprite_animation.py
+++ Группы/вт-чт_9/pyGame/6_sprite_animation.py
@@ -1,26 +1,64 @@
 import pygame as pg
 
+class AnimatedSprite(pg.sprite.Sprite):
+    def init(self, coord_x, coord_y, frames, frame_duration):
+        super().__init__()
+
+        self.frames = frames
+        self.frame_duration = frame_duration
+        self.current_frame = 0
+        self.frame_timer = 0
+
+        self.image = self.frames[self.current_frame]
+
+        self.rect = self.image.get_rect()
+        self.rect.x = coord_x
+        self.rect.y = coord_y
+
+    def update(self, dt):
+        self.frame_timer += dt
+
+        if self.frame_timer >= self.frame_duration:
+            self.frame_timer = 0
+            self.current_frame = (self.current_frame + 1) % len(self.frames)
+            self.image = self.frames[self.current_frame]
+
 pg.init()
 
 screen = pg.display.set_mode((800, 600))
 pg.display.set_caption('Анимации спрайтов')
 clock = pg.time.Clock()
 
+# Создание кадров анимации
 frames = [] # массив кадров
-for i in range(10):
+for i in range(100):
     frame = pg.Surface((200, 200))
     color = (255, i * 2.5, 0) # динамический цвет
     frame.fill(color)
     frames.append(frame) # добавляем кадр в массив кадров
 
+# Параметры анимации
+current_frame = 0
+frame_timer = 0
+frame_duration = 0.05
+
 isActive = True
 while isActive:
+    dt = clock.tick(60) / 1000
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             isActive = False
 
+    frame_timer += dt
+    if frame_timer >= frame_duration:
+        print(current_frame)
+        frame_timer = 0
+        current_frame = (current_frame + 1) % len(frames)
+
+    screen.fill((0, 0, 0))
+    screen.blit(frames[current_frame], (100, 100))
+
     pg.display.flip()
-    clock.tick(60)
 
 pg.quit()
-
