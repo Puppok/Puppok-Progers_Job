ca21e3d08af6106d00fd90104118fb11dab5751c
puppok <kravchenko19971808@gmail.com>
2026-02-25 15:11:26 +0800

    [25.02.2026] C++ 10:30-12:00

---
+++ Группы/пн-ср-10_30/C++/1_Basics.cpp
@@ -0,0 +1,34 @@
+#include <iostream> // подключение библиотеки in out stream (ввод / вывод)
+using namespace std; // использование набора стандартных команд std
+
+int main() { // главная функция программы
+    // === 1. Переменные ===
+    // тип_данных имя = значение;
+    int number = 437;
+
+    // --- Типы данных ---
+    // 1. int - целые числа
+    int num = 12;
+
+    // 2. float - дроби
+    float shell = 5.89;
+
+    // 3. double - дроби (больше диапазон значений)
+    double shell_2 = 12515.457864957;
+
+    // 4. char - один символ (одиночные кавычки)
+    char symb = 'G';
+
+    // 5. string - строки (двойные кавычки)
+    string str = "Привет, чивапчичи 288!";
+
+    // 6. bool - логический тип (true / false)
+    bool is_weekend = false;
+
+    // Принципы именования переменных
+    // getdatafromtable - очень плохо
+    // 1. Camel Case -> getDataFromTable
+    // 2. Underscore Case -> get_data_from_table
+
+    return 0; // успешное завершение программы
+}
