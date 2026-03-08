a509a502549ec1deae6394036f7e4922908f7b6d
puppok <kravchenko19971808@gmail.com>
2026-03-08 14:17:05 +0800

    [7.03.2026] С++ 14:30-17:30

---
+++ Группы/суб-14_30/C++/5_cycles.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // === Циклы ===
+    // --- 1. Цикл while ---
+    // while (условие) {
+    //      действия, выполняются пока условие = true
+    // }
+
+    return 0;
+}
