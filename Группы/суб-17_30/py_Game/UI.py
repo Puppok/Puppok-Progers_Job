f0791721cc53d6eea5a007892f97e472544a7494
puppok <kravchenko19971808@gmail.com>
2026-03-08 14:17:23 +0800

    [7.03.2026] Python (PyGame) 17:30-20:30

--- Группы/суб-17_30/py_Game/UI.py
+++ Группы/суб-17_30/py_Game/UI.py
@@ -2,6 +2,7 @@ import pygame as pg
 
 class Button:
     def __init__(self, coord_x, coord_y, width, height, text):
+        self.rect = pg.Rect(coord_x, coord_y, width, height)
         self.text = text
 
         self.color = (100, 100, 100)
@@ -13,7 +14,55 @@ class Button:
         self.is_clicked = False
 
     def update(self, mouse_pos, mouse_pressed):
-        pass
+        self.is_hovered = self.rect.collidepoint(mouse_pos)
 
-    def draw(self, screen):
-        pass
+        if self.is_hovered and mouse_pressed:
+            self.is_clicked = True
+        else:
+            self.is_clicked = False
+
+        return self.is_clicked
+
+    def draw(self, surface):
+        if self.is_clicked:
+            color = self.clicked_color
+        elif self.is_hovered:
+            color = self.hover_color
+        else:
+            color = self.color
+
+        pg.draw.rect(surface, color, self.rect, border_radius = 5)
+        pg.draw.rect(surface, (0, 0, 0), self.rect, 2, border_radius = 5)
+
+        font = pg.font.Font(None, 32)
+        text = font.render(self.text, True, self.text_color)
+        text_rect = text.get_rect(center = self.rect.center)
+        surface.blit(text, text_rect)
+
+
+pg.init()
+screen = pg.display.set_mode((800, 600))
+clock = pg.time.Clock()
+
+button = Button(300, 250, 200, 100, 'Hello')
+
+running = True
+while running:
+    clock.tick(60)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    mouse_pos = pg.mouse.get_pos()
+    mouse_pressed = pg.mouse.get_pressed()[0]
+
+    if button.update(mouse_pos, mouse_pressed):
+        print('pressed')
+
+    screen.fill((0, 0, 0))
+    button.draw(screen)
+
+    pg.display.flip()
+
+pg.quit()
