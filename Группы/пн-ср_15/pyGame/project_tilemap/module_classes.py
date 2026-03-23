227499f5b44444d4c90fb02eed60584aa397547b
puppok <kravchenko19971808@gmail.com>
2026-03-23 10:41:12 +0800

    [18.03.2026] Python (PyGame) 15:00-16:30

--- Группы/пн-ср_15/pyGame/project_tilemap/module_classes.py
+++ Группы/пн-ср_15/pyGame/project_tilemap/module_classes.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import json
 
 class TileMap:
     def __init__(self, tile_size):
@@ -19,3 +20,124 @@ class TileMap:
         # Свойства тайлов
         self.solid_tiles = {1, 3}  # Твердые
         self.danger_tiles = {4}  # Опасные
+
+    def load_from_file(self, filename):
+        with open(filename, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        self.map_data = data['map']
+        self.width = data['width']
+        self.height = data['height']
+
+    def draw(self, screen, camera_x = 0, camera_y = 0):
+        for row in range(self.height):
+            for col in range(self.width):
+                tile_id = self.map_data[row][col]
+
+                if tile_id != 0:
+                    x = col * self.tile_size - camera_x
+                    y = row * self.tile_size - camera_y
+
+                    color = self.tile_colors.get(tile_id)
+                    if color:
+                        pg.draw.rect(screen, color,(x, y, self.tile_size, self.tile_size))
+
+    def is_solid(self, x, y):
+        col = int(x // self.tile_size)
+        row = int(y // self.tile_size)
+
+        if 0 <= row < self.height and 0 <= col < self.width:
+            tile_id = self.map_data[row][col]
+            return tile_id in self.solid_tiles
+        return False
+
+    def is_danger(self, x, y):
+        col = int(x // self.tile_size)
+        row = int(y // self.tile_size)
+
+        if 0 <= row < self.height and 0 <= col < self.width:
+            tile_id = self.map_data[row][col]
+            return tile_id in self.danger_tiles
+        return False
+
+class Player:
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+
+        self.width = 28
+        self.height = 44
+
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.speed = 200
+        self.jump_power = 400
+
+        self.on_ground = False
+        self.alive = True
+
+    def update(self, dt, keys, tilemap):
+        if not self.alive:
+            return
+
+        # Управление
+        self.velocity_x = 0
+        if keys[pg.K_LEFT]:
+            self.velocity_x = -self.speed
+        if keys[pg.K_RIGHT]:
+            self.velocity_x = self.speed
+
+        # Движение X
+        self.x += self.velocity_x * dt
+
+        # Коллизия X
+        corners_x = [
+            (self.x, self.y + 5),
+            (self.x + self.width, self.y + 5),
+            (self.x, self.y + self.height - 5),
+            (self.x + self.width, self.y + self.height - 5),
+        ]
+
+        for px, py in corners_x:
+            if tilemap.is_solid(px, py):
+                if self.velocity_x > 0:
+                    self.x = int(px / tilemap.tile_size) * tilemap.tile_size - self.width - 1
+                elif self.velocity_x < 0:
+                    self.x = int(px / tilemap.tile_size) * tilemap.tile_size + tilemap.tile_size + 1
+                break
+
+        # TODO: остановились тут
+
+        # Гравитация
+        self.velocity_y += 1000 * dt
+        self.y += self.velocity_y * dt
+
+        # Коллизия Y
+        self.on_ground = False
+        corners_y = [
+            (self.x + 5, self.y),
+            (self.x + self.width - 5, self.y),
+            (self.x + 5, self.y + self.height),
+            (self.x + self.width - 5, self.y + self.height),
+        ]
+
+        for px, py in corners_y:
+            if tilemap.is_solid(px, py):
+                if self.velocity_y > 0:
+                    self.y = int(py / tilemap.tile_size) * tilemap.tile_size - self.height - 1
+                    self.velocity_y = 0
+                    self.on_ground = True
+                elif self.velocity_y < 0:
+                    self.y = int(py / tilemap.tile_size) * tilemap.tile_size + tilemap.tile_size + 1
+                    self.velocity_y = 0
+                break
+
+        # Прыжок
+        if keys[pygame.K_SPACE] and self.on_ground:
+            self.velocity_y = -self.jump_power
+
+        # Проверка опасности
+        for px, py in corners_y:
+            if tilemap.is_danger(px, py):
+                self.alive = False
+                break
