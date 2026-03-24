d7083bbe69f1e4cb4b1e5b1f8e2eab5ec52a3d2c
puppok <kravchenko19971808@gmail.com>
2026-03-24 14:52:55 +0800

    [23.03.2026] Python (PyGame) 15:00-16:30

--- Группы/пн-ср_15/pyGame/project_tilemap/module_classes.py
+++ Группы/пн-ср_15/pyGame/project_tilemap/module_classes.py
@@ -10,7 +10,7 @@ class TileMap:
 
         # Цвета тайлов
         self.tile_colors = {
-            0: None,  # Пусто
+            0: (50, 50, 50),  # Пусто
             1: (139, 69, 19),  # Земля (твердая)
             2: (0, 200, 0),  # Трава (декор)
             3: (100, 100, 100),  # Камень (твердый)
@@ -133,7 +133,7 @@ class Player:
                 break
 
         # Прыжок
-        if keys[pygame.K_SPACE] and self.on_ground:
+        if keys[pg.K_SPACE] and self.on_ground:
             self.velocity_y = -self.jump_power
 
         # Проверка опасности
@@ -141,3 +141,51 @@ class Player:
             if tilemap.is_danger(px, py):
                 self.alive = False
                 break
+
+    def draw(self, screen, camera_x = 0, camera_y = 0):
+        if self.alive:
+            color = (0, 255, 0) if self.on_ground else (0, 200, 255)
+            x = int(self.x - camera_x)
+            y = int(self.y - camera_y)
+
+            pg.draw.rect(screen, color, (x, y, self.width, self.height))
+            pg.draw.rect(screen, (0, 0, 0), (x, y, self.width, self.height), 2)
+
+class TileMapEditor:
+    def __init__(self, width, height, tile_size):
+        self.width = width  # В тайлах
+        self.height = height
+        self.tile_size = tile_size
+
+        # Пустая карта
+        self.map_data = [[0 for _ in range(width)] for _ in range(height)]
+
+        # Текущий выбранный тайл
+        self.current_tile = 1
+
+        # Смещение карты на экране
+        self.offset_x = 150
+        self.offset_y = 60
+
+        # Цвета тайлов
+        self.tile_colors = {
+            0: (50, 50, 50),  # Пусто (фон)
+            1: (139, 69, 19),  # Земля
+            2: (0, 200, 0),  # Трава
+            3: (100, 100, 100),  # Камень
+            4: (255, 0, 0),  # Вода
+        }
+
+    def handle_click(self, mouse_x, mouse_y, button): # не записали
+        # Учитываем смещение карты
+        adjusted_x = mouse_x - self.offset_x
+        adjusted_y = mouse_y - self.offset_y
+
+        col = adjusted_x // self.tile_size
+        row = adjusted_y // self.tile_size
+
+        if 0 <= col < self.width and 0 <= row < self.height:
+            if button == 1:  # ЛКМ - рисовать
+                self.map_data[row][col] = self.current_tile
+            elif button == 3:  # ПКМ - стирать
+                self.map_data[row][col] = 0
