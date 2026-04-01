89c554c6d183f2932f96196bf657d594bed6f720
puppok <kravchenko19971808@gmail.com>
2026-04-01 12:02:22 +0800

    [31.03.2026] Python (PyGame) 9:00-10:30

--- Группы/вт-чт_9/pyGame/tilemap_project/editor.py
+++ Группы/вт-чт_9/pyGame/tilemap_project/editor.py
@@ -22,3 +22,54 @@ while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+
+        # Клавиши
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_1:
+                editor.set_current_tile(1)
+            elif event.key == pg.K_2:
+                editor.set_current_tile(2)
+            elif event.key == pg.K_3:
+                editor.set_current_tile(3)
+            elif event.key == pg.K_4:
+                editor.set_current_tile(4)
+            elif event.key == pg.K_s:
+                editor.save_to_file("tilemap.json")
+            elif event.key == pg.K_l:
+                editor.load_from_file("tilemap.json")
+
+        # Мышь
+        if event.type == pg.MOUSEBUTTONDOWN:
+            mouse_pressed = True
+            editor.handle_click(*event.pos, event.button)
+
+        if event.type == pg.MOUSEBUTTONUP:
+            mouse_pressed = False
+
+    # Рисование при удержании мыши
+    if mouse_pressed:
+        mouse_buttons = pg.mouse.get_pressed()
+        mouse_pos = pg.mouse.get_pos()
+
+        if mouse_buttons[0]:  # ЛКМ
+            editor.handle_click(*mouse_pos, 1)
+        elif mouse_buttons[2]:  # ПКМ
+            editor.handle_click(*mouse_pos, 3)
+
+    # Отрисовка
+    screen.fill((30, 30, 40))
+
+    # Сдвиг для UI
+    pg.draw.rect(screen, (20, 20, 30), (0, 0, 150, 600))
+
+    # Карта
+    map_surface = pg.Surface((640, 480))
+    editor.draw_map(map_surface)
+    screen.blit(map_surface, (editor.offset_x, editor.offset_y))
+
+    # UI
+    editor.draw_ui(screen)
+
+    pg.display.flip()
+
+pg.quit()
